278d1e7d8ae4f7185a2eb663677613fdbe647cb9
Ritika <ritik@Desktop-09FNB4N>
2021-10-29 23:08:27 +0530

    Buyer Registration Use Case Completed

--- src/main/java/com/team21/controller/UserController.java
+++ src/main/java/com/team21/controller/UserController.java
@@ -1,5 +1,32 @@
 package com.team21.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team21.dto.BuyerDTO;
+import com.team21.exception.UserMSException;
+import com.team21.service.BuyerService;
+
+@RestController
 public class UserController {
 
+	@Autowired
+	BuyerService buyerService;
+
+	// Register the Buyer
+	@PostMapping(value = "/userMS/buyer/register")
+	public ResponseEntity<String> registerBuyer(@RequestBody BuyerDTO buyerDto) {
+
+		try {
+			String result = "Buyer registered successfully with buyer Id : " + buyerService.buyerRegistration(buyerDto);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (UserMSException e) {
+			String errorMsg = e.getMessage();
+			return new ResponseEntity<>(errorMsg, HttpStatus.EXPECTATION_FAILED);
+		}
+	}
 }
--- src/main/java/com/team21/service/BuyerServiceImpl.java
+++ src/main/java/com/team21/service/BuyerServiceImpl.java
@@ -1,19 +1,58 @@
 package com.team21.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.team21.dto.BuyerDTO;
+import com.team21.entity.BuyerEntity;
 import com.team21.exception.UserMSException;
 import com.team21.repository.BuyerRepository;
+import com.team21.validator.UserValidator;
 
+@Transactional
+@Service(value = "buyerService")
 public class BuyerServiceImpl implements BuyerService {
+
+	private static int buyerCount;
+	private static int sellerCount;
+
+	static {
+		buyerCount = 100;
+		sellerCount = 100;
+	}
 
 	@Autowired
 	private BuyerRepository buyerRepository;
 
 	@Override
 	public String buyerRegistration(BuyerDTO buyerDTO) throws UserMSException {
-		return null;
+
+		BuyerEntity buyer = buyerRepository.findByPhoneNumber(buyerDTO.getPhoneNumber());
+
+		if (buyer != null)
+			throw new UserMSException("Buyer already exist");
+
+		UserValidator.validateBuyer(buyerDTO);
+
+		String id = "BUY" + buyerCount++;
+
+		buyer = new BuyerEntity();
+
+		buyer.setBuyerId(id);
+		buyer.setEmail(buyerDTO.getEmail());
+		buyer.setName(buyerDTO.getName());
+		buyer.setPhoneNumber(buyerDTO.getPhoneNumber());
+		buyer.setPassword(buyerDTO.getPassword());
+		buyer.setIsActive("False");
+		buyer.setIsPrivileged("False");
+		buyer.setRewardPoints("0");
+
+		buyerRepository.save(buyer);
+
+		return buyer.getBuyerId();
+
 	}
 
 }
