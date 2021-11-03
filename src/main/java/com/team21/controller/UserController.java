4f2e2ae0347d34f13607381479d2b6159f3b7aa9
Prem Dhadkar (Personal) <prem.dhadkar@gmail.com>
2021-11-03 13:59:49 +0530

    bug fix: change rest template method from post to put

--- src/main/java/com/team21/controller/UserController.java
+++ src/main/java/com/team21/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -199,7 +200,7 @@ public class UserController {
 	}
 
 	// Remove item from Cart
-	@PostMapping(value = "/userMS/buyer/cart/remove/{buyerId}/{prodId}")
+	@PutMapping(value = "/userMS/buyer/cart/remove/{buyerId}/{prodId}")
 	public ResponseEntity<String> removeFromCart(@PathVariable String buyerId, @PathVariable String prodId)
 			throws UserMSException {
 		try {
